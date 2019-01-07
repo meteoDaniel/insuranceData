e4ed5f1eef2045674089313b4b40d6b81ab48841
meteoDaniel <daniel.lassahn@gmail.com>
2019-01-07 18:46:35 +0100

    update data

--- bin/download_aemet_monthly_data.py
+++ bin/download_aemet_monthly_data.py
@@ -1,9 +1,19 @@
 from datetime import datetime
-from src.DataHandler.aemetDataHandler import get_aemet_climate_data
+from src.DataHandler.aemetDataHandler import get_aemet_climate_data, aemet_data_processor
+from src.Statistics.statisticalCalculations import calculcate_mean_days_lower_than_table
+import pickle
+start_date = datetime(1988, 1, 1)
+end_date = datetime(2018, 3, 1)
 
-start_date = datetime(1988, 2, 1)
-end_date = datetime(1999, 12, 1)
+data = get_aemet_climate_data(start_date,
+                              end_date,
+                              'B278')
+data = aemet_data_processor(data)
+from IPython import embed
+embed()
+calculated = calculcate_mean_days_lower_than_table(data,
+                                                   2018)
 
-get_aemet_climate_data(start_date,
-                       end_date,
-                       'B278')
+pickle.dump(calculated,
+            open('/Users/imac/Desktop/Wetterberatung/insuranceDataProject/data/'
+                 'probability_data/PALMA_DE_MALLORCA_2018.p', 'wb'))
