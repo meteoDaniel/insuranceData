f8c815a75caf20fad996e4b22315d3990e131916
Daniel Lassahn <d.lassahn@meteocontrol.de>
2019-01-08 20:42:04 +0100

    Add authentication

--- src/dash/insuranceRequestApp.py
+++ src/dash/insuranceRequestApp.py
@@ -1,17 +1,28 @@
 import dash
+import dash_auth
 import os
 from datetime import datetime
 from src.dash.layoutInsuranceRequest import generate_html_layout
 from src.RequestHandler.requestHandler import RequestHandler
 from src.RequestHandler.requesHandlerServices import RequestConfig
 
-app = dash.Dash()
+VALID_USERNAME_PASSWORD_PAIRS = [
+    ['harald.brand@wiass.eu', 'brand#wiass!2019'],
+    ['forecast@event-wetter.com', 'bmtsdlewb2019']
+]
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+
 
 locations = ['PALMA_DE_MALLORCA']
 current_dir = os.getcwd()
 app.layout = generate_html_layout(locations)
 BASE_PATH_DATA = current_dir+"/../../data/probability_data/"
-
 
 
 @app.callback(dash.dependencies.Output('solution', 'children'),
@@ -36,4 +47,4 @@ def calculate_probability(trip_start_date, trip_duration, destination,
 
 
 if __name__ == '__main__':
-    app.run_server(debug=False, host="0.0.0.0", port=80)
+    app.run_server(debug=False, host="0.0.0.0", port=5000)
